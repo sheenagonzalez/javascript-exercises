57cc7205ff2621cab402bc1836edade4c694329f
Sheena Gonzalez <shngonzalez@gmail.com>
2024-10-29 12:55:22 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let result = [];
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      result.push(item);
+    }
+  })
+  return result;
 };
 
 // Do not edit below this line
