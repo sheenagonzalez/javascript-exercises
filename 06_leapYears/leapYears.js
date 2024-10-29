dc9d69cd9e42be0a8ab5fc38a615a17c3304c498
Sheena Gonzalez <shngonzalez@gmail.com>
2024-10-29 13:31:20 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isDivisibleByFourHundred = year % 400 === 0;
+  
+  if (!isDivisibleByFour || (isCentury && !isDivisibleByFourHundred)) {
+    return false;
+  }
 
+  return true;
 };
 
 // Do not edit below this line
