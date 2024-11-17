9d7c774b60ed8bad18737aa07e2cdfb642c80525
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-17 13:02:21 -0800

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.reduce((oldest, currPerson) => {
+    let currYear = new Date().getFullYear();
+    let currAge = (currPerson.yearOfDeath ? currPerson.yearOfDeath : currYear) - currPerson.yearOfBirth;
+    let oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath : currYear) - oldest.yearOfBirth;
+    return oldestAge >= currAge ? oldest : currPerson;
+  }, array[0])
 };
 
 // Do not edit below this line
