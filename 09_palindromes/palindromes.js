e53f9804350c3bf1ba071f5d48e8d830e7852ca0
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-17 03:05:23 -0800

    Complete exercse 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleaned = string
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join('');
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
