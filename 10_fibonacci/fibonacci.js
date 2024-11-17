1cf10f791020fc9881b8736a2800d4db9ef04441
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-17 12:39:54 -0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  // Returns the number in fibonacci series
+  if (n == 0) {
+    return 0;
+  }
+  if (n < 0) {
+    return 'OOPS';
+  }
+  let prev = 0;
+  let curr = 1;
+  let tempCurr;
+  for (let i = 1; i < n; i++) {
+    tempCurr = curr;
+    curr += prev;
+    prev = tempCurr;
+  }
+  return curr;
 };
 
 // Do not edit below this line
